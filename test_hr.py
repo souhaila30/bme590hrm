5f30ce53a592cc89c566d7c090bccc0abbc94403
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-02-26 14:15:34 -0500

    updated to match the class in hr.py

--- test_hr.py
+++ test_hr.py
@@ -1,48 +1,36 @@
-from hr import read_data
-from hr import read_time
-from hr import read_voltage
-from hr import ecg_plot
-from hr import find_voltage_extremes
-from hr import find_ecg_duration
+from hr import ECG
 import pytest
 
 
-ecg_data = read_data()
-
+# ecg_data = ECG.read_data(self)
+test_data1 = ECG('test_data1.csv', './test_data/')
 
 def test_read_time():
-    test_time = read_time(ecg_data)
-    assert test_time[0] == 0
+    assert test_data1.ecg_time[0] == 0
 
 
 def test_read_voltage():
-    test_voltage = read_voltage(ecg_data)
-    assert test_voltage [0] == -0.145
-
-
-# def test_ecg_plot():
-#     test_plot = ecg_plot([0,1,2,3,4],[0.1,0.2,0.3,0.4])
-#
+    assert test_data1.ecg_voltage [0] == -0.145
 
 
 def test_min_voltage():
-    test_min = find_voltage_extremes([-2,5,0,4,3,7])
-    assert test_min[0] == -2
+    #test_min = ECG.find_voltage_extremes([-2,5,0,4,3,7])
+    assert test_data1.voltage_extremes[0] == -0.68
 
 
 def test_max_voltage():
-    test_max = find_voltage_extremes([786,904,12,-0.9])
-    assert test_max[1] == 904
+    #test_max = ECG.find_voltage_extremes([786,904,12,-0.9])
+    #assert test_max[1] == 904
+    assert test_data1.voltage_extremes[1] == 1.05
 
 
 def test_voltage_tuple_size():
-    test_tuple = find_voltage_extremes([0,9.3,5,6,-3,2])
-    assert len(test_tuple) == 2
-
+    # test_tuple = ECG.find_voltage_extremes([0,9.3,5,6,-3,2])
+    # assert len(test_tuple) == 2
+    assert len(test_data1.voltage_extremes) == 2
 
 def test_ecg_duration():
-    test_duration = find_ecg_duration([9,91,904,23,54,78,891,0,85])
-    assert test_duration == 904
-
-
+    # test_duration = ECG.find_ecg_duration([9,91,904,23,54,78,891,0,85])
+    # assert test_duration == 904
+    assert test_data1.duration == 27.775
 
