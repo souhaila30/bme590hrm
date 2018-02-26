0980510053634e12274a66062701ca4b1ad5c81a
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-02-25 19:46:12 -0500

    updated test file: import error, read_time, read_voltage

---
+++ test_ecg.py
@@ -0,0 +1,36 @@
+def test_try_exception_import():
+
+    """
+
+    tests that the imported file is a csv
+
+    """
+
+    with pytest.raises(ImportError):
+        import ThisisNotAFile
+
+
+def test_read_time():
+
+    """
+
+    tests that the  time is read properly from the file
+
+    """
+
+    test_time = read_time()
+    assert test_time == time
+
+
+def test_read_voltage():
+
+    """
+
+    tests that the voltage is read properly from the file
+
+    """
+
+    test_voltage = read_voltage()
+    assert test_voltage == voltage
+
+
