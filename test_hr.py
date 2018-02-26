b3bd2f444708f00cab763dc252074a7155902df9
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-02-26 10:47:17 -0500

    tested that the correct data is read

--- test_hr.py
+++ test_hr.py
@@ -1,13 +1,21 @@
+from hr import read_data
 from hr import read_time
 from hr import read_voltage
 import pytest
 
+
+ecg_data = read_data()
+
+
 def test_read_time():
-    test_time = read_time()
-    assert test_time == 0
+    test_time = read_time(ecg_data)
+    assert test_time[0] == 0
+
 
 def test_read_voltage():
-    test_voltage = read_voltage()
-    assert test_voltage == -0.145
+    test_voltage = read_voltage(ecg_data)
+    assert test_voltage [0] == -0.145
+
+
 
 
