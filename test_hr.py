dd7eaa73005b6c55ede3ad609b2a36c5644fc19a
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-02-28 10:25:25 -0500

    test file modified to match the made up test data

--- test_hr.py
+++ test_hr.py
@@ -3,25 +3,25 @@ import pytest
 
 
 # ecg_data = ECG.read_data(self)
-test_data1 = ECG('test_data1.csv', './test_data/')
+test_data1 = ECG('test1.csv', './test_data/')
 
 def test_read_time():
     assert test_data1.ecg_time[0] == 0
 
 
 def test_read_voltage():
-    assert test_data1.ecg_voltage [0] == -0.145
+    assert test_data1.ecg_voltage [0] == 0.2
 
 
 def test_min_voltage():
     #test_min = ECG.find_voltage_extremes([-2,5,0,4,3,7])
-    assert test_data1.voltage_extremes[0] == -0.68
+    assert test_data1.voltage_extremes[0] == -0.6
 
 
 def test_max_voltage():
     #test_max = ECG.find_voltage_extremes([786,904,12,-0.9])
     #assert test_max[1] == 904
-    assert test_data1.voltage_extremes[1] == 1.05
+    assert test_data1.voltage_extremes[1] == 1
 
 
 def test_voltage_tuple_size():
@@ -32,5 +32,5 @@ def test_voltage_tuple_size():
 def test_ecg_duration():
     # test_duration = ECG.find_ecg_duration([9,91,904,23,54,78,891,0,85])
     # assert test_duration == 904
-    assert test_data1.duration == 27.775
+    assert test_data1.duration == 10
 
