eac00747f75fcb6ec0196a4fa3b05679c7cdc775
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-02-28 11:01:08 -0500

    removed unnecessary code

--- test_hr.py
+++ test_hr.py
@@ -1,8 +1,6 @@
 from hr import ECG
 import pytest
 
-
-# ecg_data = ECG.read_data(self)
 test_data1 = ECG('test1.csv', './test_data/')
 
 def test_read_time():
@@ -14,23 +12,16 @@ def test_read_voltage():
 
 
 def test_min_voltage():
-    #test_min = ECG.find_voltage_extremes([-2,5,0,4,3,7])
     assert test_data1.voltage_extremes[0] == -0.6
 
 
 def test_max_voltage():
-    #test_max = ECG.find_voltage_extremes([786,904,12,-0.9])
-    #assert test_max[1] == 904
     assert test_data1.voltage_extremes[1] == 1
 
 
 def test_voltage_tuple_size():
-    # test_tuple = ECG.find_voltage_extremes([0,9.3,5,6,-3,2])
-    # assert len(test_tuple) == 2
     assert len(test_data1.voltage_extremes) == 2
 
 def test_ecg_duration():
-    # test_duration = ECG.find_ecg_duration([9,91,904,23,54,78,891,0,85])
-    # assert test_duration == 904
     assert test_data1.duration == 10
 
