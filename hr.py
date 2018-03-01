741f69dad432d0726390d9f5d4d8b14ba3aad386
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-03-01 17:24:03 -0500

    cross correlation instead of auto-correlation, finding peaks, and calculating mean HR

--- hr.py
+++ hr.py
@@ -4,7 +4,7 @@ class ECG:
     """
 
     def __init__(self, file_type, relative_path):
-        """Initializes the attributes of Read_ecg
+        """Initializes the attributes of ECG
         """
 
         self.relative_path = relative_path
@@ -15,8 +15,10 @@ class ECG:
         self.ecg_time = 0
         self.duration = 0
         self.voltage_extremes = 0
-        #self.peaks = 0
-        self.correlation = []
+        self.correlation = 0
+        self.peaks = 0
+        self.number_beats = 0
+        self.meanHR = 0
         self.main()
 
     import logging
@@ -32,7 +34,7 @@ class ECG:
         import os
         self.input_path = os.path.join(self.relative_path,
                                        self.file_type)
-        print('input file path:', self.input_path)
+        #print('input file path:', self.input_path)
         import pandas as pd
         self.ecg_data = pd.read_csv(self.input_path, delimiter=',',
                                     header=None)
@@ -45,9 +47,9 @@ class ECG:
         :return: time of ecg data
         """
         self.ecg_time = self.ecg_data[0]
-        print('ecg time')
-        print(self.ecg_time)
-        print('first time', self.ecg_time[0])
+        #print('ecg time')
+        # print(self.ecg_time)
+        # print('first time', self.ecg_time[0])
         return self.ecg_time
 
     def read_voltage(self):
@@ -57,9 +59,9 @@ class ECG:
         :return: voltage of ecg data
         """
         self.ecg_voltage = self.ecg_data[1]
-        print('ecg voltage')
-        print(self.ecg_voltage)
-        print('first voltage', self.ecg_voltage[0])
+        # print('ecg voltage')
+        # print(self.ecg_voltage)
+        # print('first voltage', self.ecg_voltage[0])
         return self.ecg_voltage
 
     def ecg_plot(self):
@@ -86,11 +88,11 @@ class ECG:
         """
         import numpy as np
         max_voltage = np.max(self.ecg_voltage)
-        print('max voltage:', max_voltage)
+        # print('max voltage:', max_voltage)
         min_voltage = np.min(self.ecg_voltage)
-        print('min voltage:', min_voltage)
+        # print('min voltage:', min_voltage)
         self.voltage_extremes = (min_voltage, max_voltage)
-        print('voltage extremes:', self.voltage_extremes)
+        # print('voltage extremes:', self.voltage_extremes)
         return self.voltage_extremes
 
     def find_ecg_duration(self):
@@ -101,41 +103,51 @@ class ECG:
         """
         import numpy as np
         min_time = np.min(self.ecg_time)
-        print('min time:', min_time)
+        # print('min time:', min_time)
         max_time = np.max(self.ecg_time)
-        print('max time:', max_time)
+        # print('max time:', max_time)
         self.duration = max_time - min_time
-        print('duration:', self.duration)
+        # print('duration:', self.duration)
         return self.duration
 
-    def autocorrelate(self):
+    def auto_correlate(self):
         import numpy as np
         import matplotlib as mlp
         mlp.use('TkAgg')
         import matplotlib.pyplot as plt
-        self.correlation = np.correlate (self.ecg_data[1], self.ecg_data[1],'full')
-        print('ecg_data[1]',self.ecg_data[1])
+        normalized_ecg_data = (self.ecg_data - np.min(self.ecg_data))/(np.max(self.ecg_data) - np.min(
+             self.ecg_data))
+        #ecg_ = self.ecg_data[0:200]
+        #print(ecg_)
+        ecg_kernel = normalized_ecg_data[0:200]
+        #print(ecg_kernel)
+        self.correlation = np.correlate(normalized_ecg_data[1], ecg_kernel[1],'full')
         print('correlation results:', self.correlation)
         correlation_plot = plt.plot(self.correlation)
-        plt.xlabel('time')
+        plt.xlabel('lag')
         plt.ylabel('correlation sum')
         plt.show()
-        return correlation_plot
         return self.correlation
 
-    # def find_peaks(self):
-    #     """takes ecg data and finds the peaks
-    #
-    #     :param: takes in ecg data - time and voltage
-    #     :return: location of peaks - maximum voltage
-    #     """
-    #     import pandas as pd
-    #     import matplotlib as mlp
-    #     mlp.use('TkAgg')
-    #     #import matplotlib.gipyplot as plt
-    #     import numpy as np
-    #     import math
+    #def filter(self):
 
+    def find_peaks(self):
+        import numpy as np
+        from scipy import signal
+        self.peaks = signal.find_peaks_cwt(self.correlation, np.arange(1,200))
+        print('location of peaks',self.peaks)
+        return self.peaks
+
+    def count_beats(self):
+        self.number_beats= len(self.peaks)
+        print('number of beats detected:',self.number_beats)
+        return self.number_beats
+
+    def calculate_hr_bpm(self):
+        self.meanHR = self.number_beats / (self.duration / 60)
+        print('duration in seconds:',self.duration)
+        print('mean HR (bpm):', self.meanHR)
+        return self.meanHR
 
     def main(self):
         self.ecg_data = self.read_data()
@@ -144,10 +156,10 @@ class ECG:
         self.voltage_extremes = self.find_voltage_extremes()
         self.duration = self.find_ecg_duration()
         ecg_plot = self.ecg_plot()
-        #self.peaks = self.find_peaks()
-        self.correlation = self.autocorrelate()
-        correlation_plot = self.autocorrelate()
-
+        self.correlation = self.auto_correlate()
+        self.peaks = self.find_peaks()
+        self.number_beats = self.count_beats()
+        self.meanHR = self.calculate_hr_bpm()
 
 
 if __name__ == "__main__":
